8c3ec5a73a991914fd80e05ccdb557f80ec192ce
Surya <s8296659@gmail.com>
2026-06-11 19:54:24 +0530

    Update RacingSequence.tsx

--- components/RacingSequence.tsx
+++ components/RacingSequence.tsx
@@ -111,7 +111,7 @@ export default function RacingSequence({ isActive }: Props) {
         .car-layer {
           position: absolute;
           left: 50%;
-          bottom: 1%;
+          bottom: -1%;
           width: 62%;
           z-index: 3;
           transform: translateX(-50%);
