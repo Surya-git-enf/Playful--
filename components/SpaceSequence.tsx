3d9ea9be49fda1b0637fc215a14ab415e538ec22
Surya <s8296659@gmail.com>
2026-06-12 16:22:59 +0530

    Update SpaceSequence.tsx

--- components/SpaceSequence.tsx
+++ components/SpaceSequence.tsx
@@ -227,8 +227,8 @@ export default function SpaceSequence({ isActive }: Props) {
       <div style={{
         position: 'absolute',
         bottom: '10%',
-        left: '68%',
-        height: '40vh',
+        left: '69%',
+        height: '30vh',
         width: 'auto',
         zIndex: 5,
         opacity: mounted ? 1 : 0,
